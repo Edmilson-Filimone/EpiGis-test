73a0b2fe2a9035645d1be3ae781198f3c3cec377
edmilson-filimone <philimone99@gmail.com>
2023-01-20 11:13:05 +0200

    Creating basic project structure files - adding future pages and components

---
+++ epigis/src/components/CardListing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardListing() {
+  return (
+    <div>CardListing</div>
+  )
+}
+
+export default CardListing
---
+++ epigis/src/components/featuredListing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function featuredListing() {
+  return (
+    <div>featuredListing</div>
+  )
+}
+
+export default featuredListing
