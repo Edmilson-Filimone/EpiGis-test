0f64357f4de239a1e78ccddd750a5a11d2b2db07
Edmilson-Filimone <philimone99@gmail.com>
2023-01-26 00:59:21 +0200

    'Fixings the problem of fetch data and render the cards on admin page"

--- src/components/CardListing.jsx
+++ src/components/CardListing.jsx
@@ -6,10 +6,10 @@ import {FaLayerGroup} from 'react-icons/fa'
 function CardListing({id, data}) {
   return (
     <>
-      <div className="w-full bg-white shadow-md overflow-hidden">
+      <div className="w-full md:w-[320px] bg-white shadow-lg overflow-hidden">
         <div className="w-full p-2">
-          <div className="w-full h-[250px] max-h-[250px] bg-slate-600 mb-2 border">
-            <img className='w-full h-full' src={data.profileUrl} alt="Image" id="image"/>
+          <div className="w-full h-[250px] max-h-[250px] bg-slate-600 mb-2 border overflow-hidden">
+            <img className='w-full h-full transition ease-in-out duration-500 hover:scale-105' src={data.profileUrl} alt="Image" id="image"/>
           </div>
           <h3 className="mb-2 text-left font-semibold truncate card-font-h3">{data.name}</h3>
           <div className="flex flex-wrap justify-between items-center mb-2">
@@ -17,11 +17,11 @@ function CardListing({id, data}) {
               <FaLayerGroup/>
               <span className='uppercase text-[12px] font-semibold truncate'>{data.category}</span>
             </div>
-            <div className='flex items-center space-x-2 my-1.5'>
+            <div className='flex items-center space-x-2 my-1.5 w-20 overflow-hidden'>
               <GoLocation/>
               <span className='uppercase text-[12px] font-semibold truncate'>{data.district}</span>
             </div>
-            <button className="w-[80px] my-1.5 px-2 py-2 rounded-md bg-sky-400 text-white font-medium" type="button">Details</button>
+            <button className="w-[80px] my-1.5 px-2 py-1 rounded-md bg-sky-400 text-sm text-white font-medium transition ease-in-out duration-100 hover:bg-sky-500" type="button">Details</button>
           </div>
         </div>
         <div className="flex justify-between text-sm font-light mb-2 border-t pt-3 px-2">
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -7,7 +7,8 @@ import { collection, getDocs, query } from "firebase/firestore";
 
 function AdminPage() {
   const navigate = useNavigate();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [done, setDone] = useState(false)
 
   const signOut = () => {
     auth.signOut();
@@ -24,27 +25,24 @@ function AdminPage() {
       if (!querySnap.empty) {
         querySnap.forEach((doc) => {
           listDoc.push({ id: doc.id, data: doc.data() });
+          setDone(true)
+          setData(listDoc); //o estado deve ser actualizado dentro da funcao async, se colocar esse trecho fora, ele vai actualizar o estado primeiro antes de rodar a funcao - problema do assincronismo
         });
       }
     }
     fetchData();
-    setData(listDoc);
   }, []);
-
-  console.log(data);
 
   return (
     <div className="w-full mt-5">
       <h3 className="text-2xl text-center mt-5 mx-auto font-semibold">
         Administrator Page
       </h3>
-      {data && (
-        <div className="mx-auto mt-5 md:px-24 px-5 text-center grid gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-          {data.map(({ id, dados }) => (
-            <CardListing key={id} id={id} data={dados} />
-          ))}
-        </div>
-      )}
+      <div className="mx-auto mt-5 lg:px-10 px-5 text-center flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
+            {done && data.map(({id, data}) => (
+              <CardListing key={id} id={id} data={data}/>
+            ))}
+      </div>
       <button
         onClick={signOut}
         className="block mt-10 mx-auto py-2 px-3 shadow-md rounded-md bg-red-500 text-white text-center font-medium transition ease-in-out duration-100 hover:bg-red-600"
