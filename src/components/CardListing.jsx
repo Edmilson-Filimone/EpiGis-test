43245f270bff265df79cdc008f4ff38066427149
edmilson-filimone <philimone99@gmail.com>
2023-01-25 15:38:03 +0200

    Filling the admin paige with cards

--- src/components/CardListing.jsx
+++ src/components/CardListing.jsx
@@ -3,23 +3,23 @@ import {GoCalendar, GoLocation} from 'react-icons/go'
 import {FaLayerGroup} from 'react-icons/fa'
 
 
-function CardListing({url, title}) {
+function CardListing({id, data}) {
   return (
     <>
       <div className="w-full bg-white shadow-md overflow-hidden">
         <div className="w-full p-2">
           <div className="w-full h-[250px] max-h-[250px] bg-slate-600 mb-2 border">
-            <img className='w-full h-full' src={url} alt="Image" id="image"/>
+            <img className='w-full h-full' src={data.profileUrl} alt="Image" id="image"/>
           </div>
-          <h3 className="mb-2 text-left font-semibold truncate card-font-h3">{title}</h3>
+          <h3 className="mb-2 text-left font-semibold truncate card-font-h3">{data.name}</h3>
           <div className="flex flex-wrap justify-between items-center mb-2">
             <div className='flex items-center space-x-2 my-1.5'>
               <FaLayerGroup/>
-              <span className='uppercase text-[12px] font-semibold truncate'>Epidemiology</span>
+              <span className='uppercase text-[12px] font-semibold truncate'>{data.category}</span>
             </div>
             <div className='flex items-center space-x-2 my-1.5'>
               <GoLocation/>
-              <span className='uppercase text-[12px] font-semibold truncate'>Maputo</span>
+              <span className='uppercase text-[12px] font-semibold truncate'>{data.district}</span>
             </div>
             <button className="w-[80px] my-1.5 px-2 py-2 rounded-md bg-sky-400 text-white font-medium" type="button">Details</button>
           </div>
@@ -27,11 +27,11 @@ function CardListing({url, title}) {
         <div className="flex justify-between text-sm font-light mb-2 border-t pt-3 px-2">
             <div className="flex items-center space-x-2">
               <BsPersonCircle />
-              <span>Edmilson Filimone</span>
+              <span>{data.author}</span>
             </div>
             <div className="flex items-center space-x-2">
               <GoCalendar/>
-              <span>2015</span>
+              <span>{data.date}</span>
             </div>
         </div>
       </div>
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { auth } from "../firebase.config";
+import { auth, firestore } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import CardListing from "../components/CardListing";
+import { collection, getDocs, query } from "firebase/firestore";
 
 function AdminPage() {
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
 
   const signOut = () => {
     auth.signOut();
@@ -13,25 +15,36 @@ function AdminPage() {
     navigate("/");
   };
 
+  /** Fetching Data from db*/
+  useEffect(() => {
+    let listDoc = [];
+    async function fetchData() {
+      const collectionRef = collection(firestore, "maps");
+      const querySnap = await getDocs(collectionRef);
+      if (!querySnap.empty) {
+        querySnap.forEach((doc) => {
+          listDoc.push({ id: doc.id, data: doc.data() });
+        });
+      }
+    }
+    fetchData();
+    setData(listDoc);
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="w-full mt-5">
       <h3 className="text-2xl text-center mt-5 mx-auto font-semibold">
         Administrator Page
       </h3>
-      <div className="mx-auto mt-5 md:px-24 px-5 text-center grid gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-        <CardListing
-          url={"https://firebasestorage.googleapis.com/v0/b/epi-gis.appspot.com/o/Humanitarian_North_crises_2022-Final.png-02a22b5f-8f31-4f8c-ae81-7516ef669cba?alt=media&token=48637761-bf0a-40ca-8e98-de944657d32c"}
-          title={"Humanitarian north crises snapshot"}
-        />
-        <CardListing
-          url={"https://firebasestorage.googleapis.com/v0/b/epi-gis.appspot.com/o/EpiGis-2%20(2).png-ee2d710b-a926-48dc-8534-7cca77bf5076?alt=media&token=694f2eb6-ed2d-425a-bbcd-4bdd626ec265"}
-          title={"HIV/AIDS situation in Africans countries"}
-        />
-        <CardListing
-          url={"https://firebasestorage.googleapis.com/v0/b/epi-gis.appspot.com/o/Tete_Malaria_2000-2011_P.falciparum_Prevalence.png-37f30fff-ae2e-44ed-8bf0-ba8b2d7639de?alt=media&token=e7ec352e-e6d1-4768-8b0f-22b91c73de41"}
-          title={"Malaria prevalance in Tete - P. falciparum rates"}
-        />
-      </div>
+      {data && (
+        <div className="mx-auto mt-5 md:px-24 px-5 text-center grid gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+          {data.map(({ id, dados }) => (
+            <CardListing key={id} id={id} data={dados} />
+          ))}
+        </div>
+      )}
       <button
         onClick={signOut}
         className="block mt-10 mx-auto py-2 px-3 shadow-md rounded-md bg-red-500 text-white text-center font-medium transition ease-in-out duration-100 hover:bg-red-600"
