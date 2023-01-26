dbb23ae293367799201b14a3b8a6b012c882e380
Edmilson-Filimone <philimone99@gmail.com>
2023-01-26 17:33:19 +0200

    Setting the click event to the button on card component;  adding flex-shrink-0 to location icon to avoid it to shrink; wrapping the image with a anchor tag that lead to the image link

--- src/components/CardListing.jsx
+++ src/components/CardListing.jsx
@@ -1,22 +1,22 @@
 import { BsFillTrashFill, BsPencilFill, BsPersonCircle} from "react-icons/bs";
 import { GoCalendar, GoLocation } from "react-icons/go";
 import { FaLayerGroup } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function CardListing({ id, data, onDelete, onEdit }) {
   const navigate = useNavigate()
   return (
     <>
       <div className="w-full md:w-[320px] bg-white shadow-lg overflow-hidden">
-        <div className="w-full p-2" onClick={()=> navigate(`/listing/${id}`)}>
-          <div className="w-full h-[250px] max-h-[250px] bg-slate-600 mb-2 border overflow-hidden">
+        <div className="w-full p-2">
+          <a href={data.profileUrl} target="_blank" title={data.name} className="block w-full h-[250px] max-h-[250px] bg-slate-600 mb-2 border overflow-hidden">
             <img
               className="w-full h-full transition ease-in-out duration-500 hover:scale-105"
               src={data.profileUrl}
               alt="Image"
               id="image"
             />
-          </div>
+          </a>
           <h3 className="mb-2 text-left font-semibold truncate card-font-h3">
             {data.name}
           </h3>
@@ -28,7 +28,7 @@ function CardListing({ id, data, onDelete, onEdit }) {
               </span>
             </div>
             <div className="flex items-center space-x-2 my-1.5 w-20 overflow-hidden">
-              <GoLocation />
+              <GoLocation className="flex-shrink-0" />
               <span className="uppercase text-[12px] font-semibold truncate">
                 {data.district}
               </span>
@@ -36,6 +36,8 @@ function CardListing({ id, data, onDelete, onEdit }) {
             <button
               className="w-[80px] my-1.5 px-2 py-1 rounded-md bg-sky-400 text-sm text-white font-medium transition ease-in-out duration-100 hover:bg-sky-500"
               type="button"
+              onClick={()=> navigate(`/listing/${id}`)}
+              title="See more about?"
             >
               Details
             </button>
