0c86be2fce145f253a516e0687285bfedc877cc5
edmilson-filimone <philimone99@gmail.com>
2023-02-06 13:02:30 +0200

    Working on home - metrics section layout

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {ChartBarIcon, FireIcon, MapIcon, SunIcon, UserGroupIcon} from '@heroicons/react/24/outline'
 
-function Metrics() {
+function Metrics({data}) {
   return (
-    <div>Metrics</div>
+    <section className='w-screen my-16 text-white metrics-container'>
+      <section className='w-screen py-12  bg-black bg-opacity-50'>
+        <h2 className='text-3xl text-center font-medium uppercase card-font-h3 px-4'>Our collection Metrics</h2>
+        <article className='w-screen my-10'>
+          <ul className='grid gap-16 md:gap-0 md:grid-cols-2 md:grid-rows-3 lg:grid-rows-1 lg:grid-cols-5 mx-auto'>
+            <li className='metrics'>
+              <MapIcon className='metrics-icon'/>
+              <span className='metrics-value'>{data.demography + data.environment + data.epidemiology + data.humanitarian}</span>
+              <span className='metrics-label'>Total Maps</span>
+            </li>
+            <li className='metrics'>
+              <UserGroupIcon className='metrics-icon'/>
+              <span className='metrics-value'>{data.demography}</span>
+              <span className='metrics-label'>Demography</span>
+            </li>
+            <li className='metrics md:col-span-2 lg:col-auto'>
+              <SunIcon className='metrics-icon'/>
+              <span className='metrics-value'>{data.environment}</span>
+              <span className='metrics-label'>Environment</span>
+            </li>
+            <li className='metrics'>
+              <ChartBarIcon className='metrics-icon'/>
+              <span className='metrics-value'>{data.epidemiology}</span>
+              <span className='metrics-label'>Epidemiology</span>
+            </li>
+            <li className='metrics'>
+              <FireIcon className='metrics-icon'/>
+              <span className='metrics-value'>{data.humanitarian}</span>
+              <span className='metrics-label'>Humanitarian</span>
+            </li>
+          </ul>
+        </article>
+      </section>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Loading from '../components/Loading'
-import Navbar from '../components/Navbar'
-import {FaChartArea, FaChartBar, FaLeaf, FaPeopleCarry, FaSearch} from "react-icons/fa"
-import { HiOutlineUserGroup, HiUserGroup } from 'react-icons/hi'
+import {FaSearch} from "react-icons/fa"
 import Categories from '../components/Categories'
+import Metrics from '../components/Metrics'
 
 function Home() {
   const navigate = useNavigate()
@@ -17,23 +16,25 @@ function Home() {
   return (
     <main>
       <section className='hero flex flex-col items-center w-screen h-screen'>
-        <section className='mt-16 md:mt-28 py-6 px-4 hover:bg-white hover:bg-opacity-10 hover:shadow-lg transition duration-150 ease-in'>
-          <article className='mb-2'>
-            <h2 className='hero-h2 pb-4 text-7xl text-center font-bold text-white'><span className='text-blue-400'>Epi</span>Gis</h2>
-            <p className='text-2xl text-center font-medium text-white mb-4 hero-p'>Collect, analyse and inform Mozambique's indexes</p>
-            <p className='text-lg text-center font-semibold text-white hero-p'>Search for maps</p>
-          </article>
-          <form className='flex flex-col items-center w-full space-y-3 px-4' onSubmit={()=>navigate(`portfolio/${search}`)}>
-            <input className='w-full md:w-[600px] bg-transparent border border-white text-black text-center focus:bg-slate-100 focus:border-white' type="text"  placeholder='Ex: Malaria' value={search} onChange={onChange}/>
-            <button className='flex gap-2 items-center py-2 px-6 bg-blue-400 text-sm text-white font-semibold shadow-lg transition ease-out duration-150 hover:bg-blue-500 ' type="submit">
-              <FaSearch/>
-              <span>Search</span>
-            </button>
-          </form>
+        <section className='w-screen h-screen bg-black bg-opacity-5'>
+          <section className='w-fit mx-auto mt-16 md:mt-28 py-6 px-4 hover:bg-white hover:bg-opacity-10 hover:shadow-lg transition duration-150 ease-in'>
+            <article className='mb-2'>
+              <h2 className='hero-h2 pb-4 text-7xl text-center font-bold text-white'><span className='text-blue-400'>Epi</span>Gis</h2>
+              <p className='text-2xl text-center font-medium text-white mb-4 hero-p'>Collect, analyse and inform Mozambique's indexes</p>
+              <p className='text-lg text-center font-semibold text-white hero-p'>Search for maps</p>
+            </article>
+            <form className='flex flex-col items-center w-full space-y-3 px-4' onSubmit={()=>navigate(`portfolio/${search}`)}>
+              <input className='w-full md:w-[600px] bg-transparent border border-white text-black text-center focus:bg-slate-100 focus:border-white' type="text"  placeholder='Ex: Malaria' value={search} onChange={onChange}/>
+              <button className='flex gap-2 items-center py-2 px-6 bg-blue-400 text-sm text-white font-semibold shadow-lg transition ease-out duration-150 hover:bg-blue-500 ' type="submit">
+                <FaSearch/>
+                <span>Search</span>
+              </button>
+            </form>
+          </section>
+                </section>
         </section>
-      </section>
       <Categories/>
-      <section className='metrics'></section>
+      <Metrics data={{demography:0, environment:1, epidemiology:4, humanitarian:2}}/>
       <section className='portfolio'></section>
     </main>
   )
